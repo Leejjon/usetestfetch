38ecf076736e9e6dd6bf50244bbd63bf2665e7de
Leejjon <leejjon@gmail.com>
2020-02-09 23:52:00 +0100

    Wrote a test sample to retrieve GoT books.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+interface Book {
+    name: string
+}
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [books, setBooks] = useState<Book[]>([]);
+    const fetchBooks = async () => {
+        const response: Response = await fetch(
+            'https://www.anapioficeandfire.com/api/books',
+            {headers: {'Content-Type': 'application/json'}, method: "GET"});
+        setBooks(await response.json());
+    };
+
+    return (
+        <div className="App">
+            <button onClick={fetchBooks}>Get GoT books</button>
+            {books.map((book, index) => {
+                const indexToDisplay = index += 1;
+                return <div key={`book${index}`}>{indexToDisplay}&nbsp;{book.name}</div>
+            })}
+        </div>
+    );
+};
 
 export default App;
